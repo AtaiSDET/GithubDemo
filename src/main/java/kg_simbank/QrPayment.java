552fab9c8f6b97495b06215ce960a8574a2b8954
Atai Salamatov <salamatovatai@gmail.com>
2026-01-13 08:12:10 +0400

    Visa payment class added / QR code new sout added

--- src/main/java/kg_simbank/QrPayment.java
+++ src/main/java/kg_simbank/QrPayment.java
@@ -3,5 +3,6 @@ package kg_simbank;
 public class QrPayment {
     public static void main(String[] args) {
         System.out.println("QR payment");
+        System.out.println("Visa payment added");
     }
 }
