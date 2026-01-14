98f50c873bbf2dab2387eea995ed036e5857a6e0
Atai Salamatov <salamatovatai@gmail.com>
2026-01-14 08:24:44 +0400

    resolved conflict

---
+++ src/main/java/kg_simbank/delivery/Delivery.java
@@ -0,0 +1,8 @@
+package kg_simbank.delivery;
+
+public class Delivery {
+    public static void main(String[] args) {
+        System.out.println("delivery yandex");
+        System.out.println("delivery glovo");
+    }
+}
