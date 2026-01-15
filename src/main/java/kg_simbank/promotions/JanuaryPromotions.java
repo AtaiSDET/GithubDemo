44bd12265cf1468f38e9911566221a151ad34b2b
Atai Salamatov <salamatovatai@gmail.com>
2026-01-15 08:00:01 +0400

    1

---
+++ src/main/java/kg_simbank/promotions/JanuaryPromotions.java
@@ -0,0 +1,12 @@
+package kg_simbank.promotions;
+
+public class JanuaryPromotions {
+    public static void main(String[] args) {
+
+        System.out.println("January promotions");
+        System.out.println("January promotions1");
+        System.out.println("1");
+
+
+    }
+}
